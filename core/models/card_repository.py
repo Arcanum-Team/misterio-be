446729dbbe7ced5022c817b564ece76c79fefe25
Isa <isarivadero@gmail.com>
2021-11-02 23:36:28 -0300

    consulta las cartas por game_id

--- core/models/card_repository.py
+++ core/models/card_repository.py
@@ -5,6 +5,8 @@ from core.exceptions import MysteryException
 
 from core.models.card_model import Card
 from core.settings import logger
+from core.models.player_repository import find_player_by_id
+from core.models.games_repository import find_game_by_id
 
 cards = [ #monsters
          ["DRACULA", "MONSTER"],["FRANKENSTEIN", "MONSTER"],["HOMBRE_LOBO", "MONSTER"], 
@@ -26,3 +28,18 @@ def initializeCards():
 @db_session
 def get_cards():
     return select((c.id, c.name, c.type) for c in Card)[:]
+
+@db_session
+def get_cards_by_player_id(id_player):
+    player_by_id= find_player_by_id(id_player)
+    if player_by_id.cards:
+        return select((c.id, c.name, c.type) for c in player_by_id.cards)[:]
+    return "this player does't have any card assigned yet"
+
+@db_session
+def get_cards_by_game_id(id_game):
+    game_by_id= find_game_by_id(id_game)
+    if game_by_id:
+        players_id= select(p.id for p in game_by_id.players)[:]
+        return [get_cards_by_player_id(id_player) for id_player in players_id]
+    return "this game does't exist yet"
--- v1/endpoints/cards_endpoints.py
+++ v1/endpoints/cards_endpoints.py
@@ -4,6 +4,8 @@ from typing import List
 from fastapi import APIRouter
 from core.schemas.card_schema import CardBasicInfo
 from core.models.card_repository import get_cards, initializeCards
+from core.models.card_repository import get_cards_by_game_id
+
 
 cards_router = APIRouter()
 
@@ -15,3 +17,13 @@ def get_all_cards():
         cards = get_cards()
     
     return [CardBasicInfo(id=c[0], name=c[1], type=c[2]) for c in cards]
+
+
+@cards_router.get("/{id}", response_model= List[CardBasicInfo])
+def get_each_player_cards(id: UUID):
+    cards_by_game= get_cards_by_game_id(id)
+
+
+
+    return [CardBasicInfo(id=c[0], name=c[1], type=c[2]) for c in cards_by_game]
+
