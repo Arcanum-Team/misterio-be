49866207a8b715d57181ad8591d4bf6e71edec44
Benja272 <bm0130647@gmail.com>
2021-11-05 21:03:39 -0300

    crear tablero incompleto

--- v1/endpoints/__init__.py
+++ v1/endpoints/__init__.py
@@ -1,3 +1,5 @@
+from pony.utils.utils import import_module
 from .shifts_enpoint import shifts_router
 from .games_endpoint import games_router
 from .cards_endpoints import cards_router
+from .board_endpoint import board_router
