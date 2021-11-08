c8b4a2df59d6eab001294af66037d2700316c969
Fernando Pérez <fernandorap@ext.inditex.com>
2021-11-08 20:24:38 -0300

    using repository method

--- core/repositories/card_repository.py
+++ core/repositories/card_repository.py
@@ -4,13 +4,14 @@ from core.models.card_model import Card
 
 @db_session
 def get_cards():
-    return select((c.id, c.name, c.type) for c in Card)[:]
+    return Card.select()
 
 
 @db_session
 def get_card_info_by_id(card_id):
-    c = Card.get(id = card_id)
+    c = Card.get(id=card_id)
     return (c.id, c.name, c.type)
+
 
 @db_session
 def get_card(card_id):
