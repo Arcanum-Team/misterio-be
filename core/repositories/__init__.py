00d3aff052102d5a8fa1c6e53a3b545750507cc7
Benja272 <bm0130647@gmail.com>
2021-11-13 18:50:48 -0300

    fix

--- core/repositories/__init__.py
+++ core/repositories/__init__.py
@@ -1,5 +1,5 @@
 from .board_repository import get_all_boxes, get_box_by_id, get_box_type_by_id, get_enclosure_by_id, get_complete_row, \
-    get_adjacent_boxes, get_boxes_by_type, get_adj_special_box, is_trap
+    get_adjacent_boxes, get_boxes_by_type, get_adj_special_box, is_trap, find_four_traps
 from .card_repository import get_card_info_by_id, get_cards, get_card_by_id
 from .games_repository import join_player_to_game, get_games, new_game, start_game, find_complete_game, pass_turn, \
     find_game_by_id, find_player_by_id_and_game_id 
